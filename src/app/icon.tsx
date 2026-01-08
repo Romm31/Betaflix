c5657c368c942e8521b958419cb17ca58a36674b
Romm31 <ichbinerwin99@gmail.com>
2026-01-08 23:04:40 +0700

    update

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -18,46 +18,19 @@ export default function Icon() {
       <div
         style={{
           fontSize: 24,
-          background: '#800000',
+          background: '#E50914', // Betaflix Red
           width: '100%',
           height: '100%',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          color: '#C5A059',
-          borderRadius: 8,
-          position: 'relative',
+          color: '#FFFFFF',
+          borderRadius: 4, // Slightly rounded square
+          fontWeight: 900,
+          fontFamily: 'sans-serif',
         }}
       >
-        {/* Abstract pattern lines */}
-        <div
-            style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                border: '1px solid rgba(255,255,255,0.1)',
-                borderRadius: 8,
-            }}
-        />
-        {/* Simple B icon */}
-        <div style={{ fontWeight: 900, fontFamily: 'sans-serif', marginTop: -2 }}>
-            B
-        </div>
-        {/* Play triangle accent */}
-        <div
-            style={{
-                position: 'absolute',
-                right: 4,
-                bottom: 4,
-                width: 0,
-                height: 0,
-                borderLeft: '6px solid #C5A059',
-                borderTop: '3px solid transparent',
-                borderBottom: '3px solid transparent',
-            }}
-        />
+        B
       </div>
     ),
     // ImageResponse options
